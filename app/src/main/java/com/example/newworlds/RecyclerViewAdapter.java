280e9136fedd8e8553078628b63641b4322c247e
eallen26 <eallen@wpi.edu>
2019-05-06 16:07:54 -0400

    New RecyclerViewAdapter file

--- app/src/main/java/com/example/newworlds/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/newworlds/RecyclerViewAdapter.java
@@ -1,12 +1,17 @@
 package com.example.newworlds;
 
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.support.v7.widget.LinearLayoutManager;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -17,65 +22,48 @@ import java.util.ArrayList;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     public static final String TAG="RecyclerViewAdapter";
 
-    private String stringEden = new String();
-    //private ArrayList<String> mPreferencesCategories=new ArrayList<>();
-    //private Spinner mChoices = new Spinner();
+    private ArrayList<Resturant> allRestaurants;
     private Context mContext;
 
-    /*
-    public RecyclerViewAdapter(ArrayList<String> preferencesCategories, Spinner choices,Context context){
-        //mPreferencesCategories=preferencesCategories;
-        //mChoices=choices;
-        mContext=context;
-    }
-    */
-    public RecyclerViewAdapter(String eden,Context context){
-        //mPreferencesCategories=preferencesCategories;
-        //mChoices=choices;
-        stringEden=eden;
+    public RecyclerViewAdapter(ArrayList<Resturant> data, Context context){
+        allRestaurants=data;
         mContext=context;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem,parent,false);
+        LinearLayout view = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_card,parent,false);
+        CardView cardView = view.findViewById(R.id.cardRestaurant);
         ViewHolder holder = new ViewHolder(view);
-        return null;
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         Log.d(TAG,"onBindViewHolder: called");
-
-        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Log.d(TAG,"onClick: clicked on: " + mChoices.get(position));
-                Log.d(TAG,"onClick: clicked on: " + stringEden);
-                Toast.makeText(mContext,stringEden,Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
+        Resturant restaurant = allRestaurants.get(position);
+        holder.restaurantName.setText(restaurant.getName());
+        holder.restaurantType.setText(restaurant.getType());
+        holder.restaurantAddress.setText(restaurant.getAddress());
     }
 
     @Override
 
+
     public int getItemCount() {
-        return stringEden.length();
+        return allRestaurants.size();
     }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView food;
-        Spinner foodChoices;
-        RelativeLayout parentLayout;
+        TextView restaurantName, restaurantType, restaurantAddress;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull LinearLayout itemView) {
             super(itemView);
-            food =itemView.findViewById(R.id.food);
-            foodChoices=itemView.findViewById(R.id.foodChoices);
-            parentLayout=itemView.findViewById(R.id.parent_layout);
+            restaurantName = itemView.findViewById(R.id.restaurantName);
+            restaurantType = itemView.findViewById(R.id.restaurantType);
+            restaurantAddress = itemView.findViewById(R.id.address);
         }
     }
 }
