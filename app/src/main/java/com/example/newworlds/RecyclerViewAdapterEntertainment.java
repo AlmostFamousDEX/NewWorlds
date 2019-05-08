6981229edc43a60daf34c40897a6593346642ff8
eallen26 <eallen@wpi.edu>
2019-05-08 19:39:30 -0400

    RecyclerViewAdapterEntertainment

--- app/src/main/java/com/example/newworlds/RecyclerViewAdapterEntertainment.java
+++ app/src/main/java/com/example/newworlds/RecyclerViewAdapterEntertainment.java
@@ -9,7 +9,9 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,34 +19,14 @@ import java.util.ArrayList;
 
 public class RecyclerViewAdapterEntertainment extends RecyclerView.Adapter<RecyclerViewAdapterEntertainment.ViewHolder> {
     public static final String TAG="RecyclerViewAdapter";
-
-    //final Controller aController = (Controller) getApplicationContext();
-    // ArrayList<Resturant> restaurantData = aController.getRestaurants();
-    //private ArrayList<Resturant>myDataSet2;
     private ArrayList<Entertainment> allEntertainment;
-    //private ArrayList<String> mPreferencesCategories=new ArrayList<>();
-    //private Spinner mChoices = new Spinner();
-    //private String stringEden;
+    private ArrayList<Entertainment> itineraryEntertainment;
     private Context mContext;
 
-    /*
-    public RecyclerViewAdapter(ArrayList<String> preferencesCategories, Spinner choices,Context context){
-        //mPreferencesCategories=preferencesCategories;
-        //mChoices=choices;
-        mContext=context;
-    }
-    */
     public RecyclerViewAdapterEntertainment(ArrayList<Entertainment> data, Context context){
-        //mPreferencesCategories=preferencesCategories;
-        //mChoices=choices;
-        //restaurantData=idk;
-        //myDataSet2=idk2;
         allEntertainment=data;
         mContext=context;
     }
-
-    //public RecyclerViewAdapter( RecyclerActivity recyclerActivity) {
-    //}
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -59,25 +41,31 @@ public class RecyclerViewAdapterEntertainment extends RecyclerView.Adapter<Recyc
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         Log.d(TAG,"onBindViewHolder: called");
         Entertainment entertainment = allEntertainment.get(position);
-        //Entertainment entertainment = allEntertainments.get(position);
-        //Education education = allEducations.get(position);
         holder.Name.setText(entertainment.getName());
         holder.Type.setText(entertainment.getType());
         holder.Address.setText(entertainment.getAddress());
+/*
+        holder.selectEntertainment.setOnClickListener(new View.onClickListener() {
+            public void onClick(View view) {
+                if (((CheckBox) view).isChecked()) {
+                    itineraryEntertainment.add(allEntertainment.get(getAdapterPosition()));
+                }
+            }
+        });
+        //we still have to debug the onClick method and onClickListeners for the check boxes to work.
+        */
     }
 
     @Override
-
-
     public int getItemCount() {
         return allEntertainment.size();
         //return myDataSet2.size();
     }
 
-
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView Name, Type, Address;
+        CheckBox selectEntertainment;
 
         public ViewHolder(@NonNull LinearLayout itemView) {
             super(itemView);
@@ -86,5 +74,4 @@ public class RecyclerViewAdapterEntertainment extends RecyclerView.Adapter<Recyc
             Address = itemView.findViewById(R.id.address);
         }
     }
-
 }
