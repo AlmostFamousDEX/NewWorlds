e191a2b70b24d8764faa2dc0cfcaec448438acdf
drenyob <drenyob@gmail.com>
2019-05-15 00:23:24 -0400

    Alex Test 2

--- app/src/main/java/com/example/newworlds/Controller.java
+++ app/src/main/java/com/example/newworlds/Controller.java
@@ -17,6 +17,9 @@ import java.util.Arrays;
 import static android.media.CamcorderProfile.get;
 
 public class Controller extends Application {
+
+    //List of private variable
+
     private ArrayList<Resturant> resturants = new ArrayList<Resturant>();
     private ArrayList<Entertainment> entertainment = new ArrayList<Entertainment>();
     private ArrayList<Education> education = new ArrayList<Education>();
@@ -44,9 +47,12 @@ public class Controller extends Application {
 
     private ArrayList<String> dummy2 = new ArrayList<String>();
 
+    private ArrayList<Itenerary> alexDummyIRanOutOfOriginalNames = new ArrayList<Itenerary>();
+
     public void onCreate() {
 
         super.onCreate();
+        //run this method when MainActivity is created
 
         resturants();
         entertainment();
@@ -55,11 +61,14 @@ public class Controller extends Application {
         Log.d("AlexTestController","Good");
 
     }
+
+    //Next three methods fill out original class arrays
     public void resturants(){
         InputStream is = getResources().openRawResource(R.raw.restaurants);
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
-
+        //This variable is created to skip the first line of the CSV file
+        //Same for next two methods
         boolean skip =false;
 
         String line ="";
@@ -78,6 +87,7 @@ public class Controller extends Application {
                 }
             }
         }
+        //catch exceptions
         catch (IOException e){
             Log.wtf("MainActivity-restaurants","ERROR reading data on line: "+line);
         }
@@ -136,14 +146,17 @@ public class Controller extends Application {
             Log.wtf("MainActivity","ERROR reading data on line: "+line);
         }
     }
+    //returns a sorted array of education types
     public String[] getEducationType(){
         String[] Sites = new String[education.size()];
         for (int i=0; i<education.size(); i++)
         {
             Education site = education.get(i);
             Sites[i]=site.getType();
+            //populate Sites[]
         }
         Arrays.sort(Sites);
+        //Sort array
         int delete3=0;
 
         for (int i=1; i<Sites.length; i++)
@@ -151,6 +164,7 @@ public class Controller extends Application {
             if (Sites[i].equals(Sites[i-1])) {
                 Log.d("a repeat!!","yeee");
                 Sites[i] = "XX";
+                //If an element in the sites array is the same as the one before it, change it to XX
             }
             else{
                 Log.d("not a repeat","yerrrr");
@@ -159,24 +173,27 @@ public class Controller extends Application {
         for (int l=0;l<Sites.length;l++){
             if (Sites[l].equals(("XX"))){
                 delete3++;
+                //find number of elements that are XX
             }
         }
         String[] sites = new String[Sites.length-delete3+1];
         sites[0]="None Selected";
+        //create new array with none selected as first element
 
         int counter3=1;
 
         for (int j=0;j<Sites.length;j++)
         {
-            if (!(Sites[j].equals("XX")))
+            if (!(Sites[j].equals("XX"))) //If element in Sites[] is not XX assign it to a corresponding value in sites[] and increment a counter
             {
                 sites[counter3]=Sites[j];
                 counter3++;
             }
         }
 
-        return sites;
+        return sites; //output
     }
+    //For the next two methods see previous
     public String[] getResturantType(){
         String[] Sites = new String[resturants.size()];
         for (int i=0; i<resturants.size(); i++)
@@ -313,6 +330,8 @@ public class Controller extends Application {
         return dummy;
     }
 
+    //returns a String[] of entertainment venues of a certain type
+
     public String[] getNameFromEntertainmentType(String type)
     {
         int counter=0;
@@ -338,6 +357,8 @@ public class Controller extends Application {
         return dummy;
     }
 
+    //get info for res based off name
+
     public String[] getInfoResturant(String name)
     {
         String[] dummy = new String[3];
@@ -353,6 +374,8 @@ public class Controller extends Application {
         }
         return dummy;
     }
+
+    //see above for next two methods
 
     public String[] getInfoEntertainment(String name)
     {
@@ -386,6 +409,8 @@ public class Controller extends Application {
         return dummy;
     }
 
+    //add object to an ArrayList of resturants that will be used to display the itenerary
+
     public void addObjectToResturantDisplay( String name)
     {
         for (int i=0; i<resturants.size(); i++)
@@ -397,9 +422,13 @@ public class Controller extends Application {
         }
     }
 
+    //get Array list
+
     public ArrayList<Resturant> getResturantsDisplay() {
         return resturantsDisplay;
     }
+
+    //For next 4 see previous 2
 
     public void addObjectToEducationDisplay(String name)
     {
@@ -431,6 +460,11 @@ public class Controller extends Application {
     public ArrayList<Entertainment> getEntertainmentDisplay() {
         return entertainmentDisplay;
     }
+
+    //This next method is misnamed OPPS!
+    //It should read getResturants from type
+    //Basically it adds all resturants of a certain type to an ArrayList
+    //Next 2 also do the same thing for different objects
 
     public ArrayList<Resturant> getResturantsFromName(String type)
     {
@@ -468,6 +502,9 @@ public class Controller extends Application {
         return edenEntertainment;
     }
 
+    //Stores the String off current education type
+    //Next 2 follow similar pattern
+
     public void setCurrentEducationType(String currentEducationType) {
         this.currentEducationType = currentEducationType;
     }
@@ -479,6 +516,9 @@ public class Controller extends Application {
     public void setCurrentEntertainmentType(String currentEntertainmentType) {
         this.currentEntertainmentType = currentEntertainmentType;
     }
+
+    //If there is a current education type return it, otherwise return an error message
+    //Same for next two
 
     public String getCurrentEducationType() {
         if (currentEducationType!=null)
@@ -516,9 +556,13 @@ public class Controller extends Application {
         }
     }
 
+    //permuatation method to show all permutations of 3 given sets of ArrayLists
+
     public ArrayList<ArrayList<Itenerary>> permutate(ArrayList<Resturant> res, ArrayList<Education> edu, ArrayList<Entertainment> ent)
     {
         int counter=0;
+
+        //See how many ArrayLists contain stuff
 
         if (res.size()>0)
         {
@@ -533,9 +577,11 @@ public class Controller extends Application {
             counter++;
         }
 
+        //create dummy variable
+
         ArrayList<ArrayList<Itenerary>> dummy =new ArrayList<ArrayList<Itenerary>>();
 
-        if (counter==3)
+        if (counter==3)//if 3 ArrayLists contain stuff...
         {
             for (int i=0; i<res.size(); i++)
             {
@@ -543,6 +589,7 @@ public class Controller extends Application {
                 {
                     for (int k=0; k<ent.size(); k++)
                     {
+                        //loop though each possibility and convert the individual elements selcted to Itenerary objects
                         Itenerary it1 = new Itenerary(res.get(i).getName(),res.get(i).getType(),res.get(i).getAddress(),res.get(i).getOhours(),res.get(i).getChours());
                         Itenerary it2 = new Itenerary(edu.get(j).getName(),edu.get(j).getType(),edu.get(j).getAddress(),edu.get(j).getOhours(),edu.get(j).getChours());
                         Itenerary it3 = new Itenerary(ent.get(k).getName(),ent.get(k).getType(),ent.get(k).getAddress(),ent.get(k).getOhours(),ent.get(k).getChours());
@@ -554,12 +601,14 @@ public class Controller extends Application {
                         itOfficial.add(it3);
 
                         dummy.add(itOfficial);
+                        //add the combination just made to the dummy variable
                     }
                 }
             }
             return dummy;
         }
-        if (counter==2)
+        if (counter==2)//same idea for this case as well
+                        //The main differnce is th I now need to figure out which one does not contain stuff and loop accordingly
         {
             if (res.size()==0)
             {
@@ -628,7 +677,7 @@ public class Controller extends Application {
             }
             return dummy;
         }
-        if (counter==1)
+        if (counter==1)//Only have to figure out which one contains stuff
         {
             if (res.size()!=0)
             {
@@ -687,6 +736,9 @@ public class Controller extends Application {
         return dummy;
     }
 
+    //Returns entertainment object based off a name
+    //Same for next 2 (different class types)
+
     public Entertainment getObjectFromEntertainmentName(String name)
     {
         for (int i=0; i<entertainment.size(); i++)
@@ -723,27 +775,30 @@ public class Controller extends Application {
         return null;
     }
 
+    //adds items to ArrayList when given an array list of names
+    //Same for next 2
+
     public void addItemToArjunResturant (ArrayList<String> res)
     {
-        for (int i=0; i<res.size(); i++)
+        for (int i=0; i<res.size(); i++)//loop thru given array list
         {
             String dummy = res.get(i);
             boolean test = true;
 
-            for (int k=0; k<arjunResturant.size(); k++)
+            for (int k=0; k<arjunResturant.size(); k++)//make sure the element we are adding does not already exist in the ArrayList
             {
                 if (arjunResturant.get(k).getName().equals(dummy))
                 {
                     test=false;
                 }
             }
-            if (test)
+            if (test)//if test passes
             {
                 for (int k=0; k<resturants.size(); k++)
                 {
                     if (resturants.get(k).getName().equals(dummy))
                     {
-                        arjunResturant.add(resturants.get(k));
+                        arjunResturant.add(resturants.get(k));//add item
                     }
                 }
             }
@@ -802,6 +857,8 @@ public class Controller extends Application {
         }
     }
 
+    //get metrhods
+
     public ArrayList<Education> getArjunEducation() {
         return arjunEducation;
     }
@@ -813,6 +870,8 @@ public class Controller extends Application {
     public ArrayList<Resturant> getArjunResturant() {
         return arjunResturant;
     }
+
+    //remove element form ArrayList based off name
 
     public void removeElementFromArjunResturant (String name)
     {
@@ -846,6 +905,8 @@ public class Controller extends Application {
         }
     }
 
+    //setter and getter for Itenerary ArrayList
+
     public void setIt(ArrayList<Itenerary> it) {
         this.it = it;
     }
@@ -854,11 +915,13 @@ public class Controller extends Application {
         return it;
     }
 
-
+    //permutation method for an Itenerary ArrayList
     public ArrayList<ArrayList<Itenerary>> permutate2(ArrayList<Itenerary> it)
     {
-        dummy2.clear();
+        dummy2.clear(); //clear ArrayList used to store permutations
         ArrayList<ArrayList<Itenerary>> dummy = new ArrayList<ArrayList<Itenerary>>();
+
+        //determine size and then get permutation possibilities
         if (it.size()==1)
         {
             permutate3("","0");
@@ -873,30 +936,45 @@ public class Controller extends Application {
         }
         for (int i=0; i<dummy2.size(); i++)
         {
-            ArrayList<Itenerary> alexDummyIRanOutOfOriginalNames = new ArrayList<Itenerary>();
+            alexDummyIRanOutOfOriginalNames.clear();
             String str = dummy2.get(i);
+            Log.d("AlexSaysWork",str);
+            //loop to get each permutation
             for (int k=0; k<str.length(); k++)
             {
+                /*
+                * This works because each permutation is essentially 012, 021, 102 ...
+                * What I do is to convert the string into characters
+                * Each of these characters are converted to an int which is then used to reference
+                 * the original ArrayList passed as an argument
+                *
+                * */
                 char c = str.charAt(k);
-                int z = (int)(c);
+                int z = Character.getNumericValue(c);
                 alexDummyIRanOutOfOriginalNames.add(it.get(z));
             }
             dummy.add(alexDummyIRanOutOfOriginalNames);
-            alexDummyIRanOutOfOriginalNames.clear();
+
         }
         return dummy;
     }
 
-    public void permutate3(String pre, String rest) {
+    //Recursive method that stores all permutations of a String in an Arraylist
+
+    private void permutate3(String pre, String rest) {
         if (rest.length()==1)
         {
             String str = pre+rest;
             dummy2.add(str);
+            //add Strings to String ArrayList
         }
         else
         {
             for (int i=0; i<rest.length(); i++)
             {
+                //loop thru all characters in remaining string
+                //select character, add it to pre and the delete it from rest
+                //then recursively call the method
                 char c = rest.charAt(i);
                 String str1 = String.valueOf(c);
                 String str2=pre+str1;
@@ -935,6 +1013,8 @@ public class Controller extends Application {
         return combinations;
     }
 */
+
+    //setter and getter method
     public void addItemToAlexsItenerary(ArrayList<Itenerary> itLives)
     {
         alexsItenerary.add(itLives);
